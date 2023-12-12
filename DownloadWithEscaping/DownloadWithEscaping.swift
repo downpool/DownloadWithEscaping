1917f1f6bc0811c9d7ff3728d9520d43a842171d
donghyeon choi <downpool.choi@gmail.com>
2023-12-12 19:30:10 +0900

    Initial commit

---
+++ DownloadWithEscaping/DownloadWithEscaping.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  DownloadWithEscaping
+//
+//  Created by donghyeon choi on 12/12/23.
+//
+
+import SwiftUI
+
+struct PostModel: Codable {
+    
+}
+
+class DownloadWithEscapingViewModel: ObservableObject {
+
+    @Published var posts: [PostModel] = []
+    
+    init() {
+        getPost()
+    }
+    
+    func getPost() {
+        guard let url = URL(string: "") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+        }
+    }
+}
+
+struct DownloadWithEscaping: View {
+    
+    @StateObject var vm = DownloadWithEscapingViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+    
+    }
+}
+
+#Preview {
+    DownloadWithEscaping()
+}
---
+++ DownloadWithEscaping/DownloadWithEscapingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadWithEscapingApp.swift
+//  DownloadWithEscaping
+//
+//  Created by donghyeon choi on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DownloadWithEscapingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DownloadWithEscaping()
+        }
+    }
+}
